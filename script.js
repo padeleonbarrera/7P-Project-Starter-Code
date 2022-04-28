d59d662eed51e61cb470b4d7ae7cec3e9d917903
padeleonbarrera <104597514+padeleonbarrera@users.noreply.github.com>
2022-04-28 14:05:03 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,6 @@
+$("button").click(function(){
+ let variable1 =$(".1").val();
+  let variable2 =$(".2").val();
+  let variable3 =$(".3").val();
+    console.log(variable1,variable2,variable3,);
+});
